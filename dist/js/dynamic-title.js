9555e7b5e107ba51467b00b7db9b961028680bec
afei <88603982@qq.com>
2016-05-15 22:46:00 +0800

    initial

---
+++ dist/js/dynamic-title.js
@@ -0,0 +1,35 @@
+(function () {
+  'use strict';
+
+  angular.module('nx.widget', []);
+
+})();
+
+(function () {
+  'use strict';
+
+  angular.module('nx.widget')
+    .directive('dynamicTitle', dynamicTitle);
+
+  /** @ngInject */
+  function dynamicTitle($rootScope, $timeout) {
+    return {
+      restrict: 'A',
+      link: function (scope, element) {
+
+        var listener = function (event, toState) {
+
+          var title = 'MyTitle';
+          if (toState.data && toState.data.pageTitle) title = toState.data.pageTitle;
+
+          $timeout(function () {
+            element.text(title);
+          }, 0, false);
+        };
+
+        $rootScope.$on('$stateChangeSuccess', listener);
+      }
+    };
+  }
+
+})();
---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+/**
+ *  Welcome to your gulpfile!
+ *  The gulp tasks are splitted in several files in the gulp directory
+ *  because putting all here was really too long
+ */
+
+'use strict';
+
+var gulp = require('gulp');
+var path = require('path');
+var del = require('del');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+
+var scripts = [
+  './src/angular/main.js',
+  './src/angular/directive/dynamic-title.js'
+];
+
+
+gulp.task('clean', function () {
+  return del('dist');
+});
+
+
+gulp.task('scripts', function () {
+  return gulp.src(scripts)
+    .pipe(concat('dynamic-title.js'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: ".min",
+      extname: ".js"
+    }))
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('default', ['clean'], function () {
+  gulp.start([ 'scripts']);
+});
